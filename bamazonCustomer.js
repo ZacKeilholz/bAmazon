e73903ff1f23e27e49b8a7467b3d76e19c836d09
zac keilholz <keilh001@umn.edu>
2018-06-06 13:49:17 -0500

    bamazonCustomer is MVP is ready- would like to add Table Display in the future

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -52,59 +52,148 @@ connection.connect(function (err) {
     start();
 })
 
-//MAIN CODE- Inquirer Prompt
+//MAIN CODE- Inquirer Prompt - Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
 //===================================
 function start() {
 
-    connection.query("select product_name, department_name, price from products WHERE stock_quantity > 0", function (err, res) {
+    //Retrieve Up to Date Product Info From SQL Database
+    connection.query("SELECT item_id, product_name, price FROM products WHERE stock_quantity > 0", function (err, res) {
         if (err) throw err;
-        console.log(res);
-        console.log(typeof res);
-        var productArr = [];
-        res.forEach(function(element) {
-            productArr.push(element.product_name)
-            ;
-        });
-        console.log(productArr);
 
-        inquirer
-            .prompt({
-                name: "choice",
-                type: "list",
-                message: "Welcome to Bamazon.  Select the Item you would like to buy.",
-                choices: productArr
-            })
-            .then(function (answer) {
-                console.log(answer.productList);
-            });
+        console.log(`Welcome to Bamazon.  Select the Item you would like to buy; Items are sorted by: ID  |  Product Name   |  Price`);
+
+        //Display List of Items for Sale
+        console.log(displayItems(res));
+
+        //Get Product ID and QTTY from User for Items they would like to purchase using Inquirer
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "id",
+                message: "Please Enter the ID of the Product You Would Like to Purchase",
+
+                //Validate the Input
+                validate: function (value) {
+                    if (isNaN(value) || parseInt(value) > res.length || parseInt(value) < 0) {
+                        return "Please Provide a Valid Product ID";
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "qty",
+                message: "How much would you like to purchase?",
+                validate: function (value) {
+                    if (isNaN(value)) {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            }
+        ]).then(function (answer) {
+            checkAmounts(answer);
+        });
     });
 };
 
 
-function checkConnection() {
-    connection.query("SELECT * FROM products", function (err, res) {
+function checkAmounts(value) {
+    // Check to see if quantity is available
+    connection.query(`select product_name, stock_quantity, price from products WHERE item_id = ${value.id}`, function (err, res) {
+        //error Check
         if (err) throw err;
-        console.log(res);
 
+        //Create Variables for retrieved SQL Info for later use
+        var sqlProductName = res[0].product_name;
+        var sqlProductQuantity = res[0].stock_quantity;
+        var sqlProductPrice = res[0].price;
+
+        //Check if there is enough in stock to match requested customer qtty
+        if (value.qty <= res[0].stock_quantity) {
+
+            //Updated Quantity to be sent into the UPDATE sql query
+            var sqlUpdatedQuantity = parseInt(sqlProductQuantity) - parseInt(value.qty);
+
+            console.log(`Hell Yes! You just purchased ${value.qty} of ${sqlProductName} for $ ${(value.qty * sqlProductPrice).toFixed(2)}
+            \nWould you Like to Buy Something Else?`);
+
+            //Update SQL Database Quantities
+            updateSQL(value.id, sqlUpdatedQuantity);
+
+            //Prompt user to Play again
+            playAgain();
+
+        } else {
+            //If there is Not enough in stock:
+            console.log(`"Not enough of this item in stock!- Current Inventory of : ${res[0].stock_quantity}`);
+
+            //Restart Bamazon Prompt
+            start();
+        }
     });
 }
 
+//Prompt User via Inquirer to Purchase More Items
+function playAgain() {
 
-function checkItem() {
+    //Create Inquirer Question
+    var question =
+        [
+            {
+                type: "list",
+                name: "again",
+                message: "Would you like to purchase something else?",
+                choices: ['Yes', 'No']
+            }
+        ];
 
-    var sqlQuery = "SELECT * FROM products where quantity < 5";
-
-    connection.query(sqlQuery, function (err, result) {
-        if (err) throw err;
-        console.log("ITEMS WITH QTTY UNDER 5");
-
-
-        console.log(results);
-        console.log(result.forEach(function (element) {
-            console.log(element.name);
-        }));
+    inquirer.prompt(question).then(answers => {
+        if (answers.again == 'Yes') {
+            console.log("Let's Purchase Something Else");
+            start();
+        } else {
+            console.log(`
+            \nThank you for shopping Bamazon!  
+            \nGoodbye!`);
+        }
     });
 }
+
+//Updates the SQL products table to reflect the change in quantity 
+function updateSQL(id, quantity) {
+
+    var query = connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity: quantity
+            },
+            {
+                item_id: id
+            }
+        ],
+        function (err, res) {
+            if (err) throw err;
+        });
+
+    //Debug Query Output
+    //console.log(query.sql);
+}
+
+
+//Takes MySQL Result and Displays all items for sale (***USE NPM TABLE TO FORMAT THIS IF THERE IS EXTRA TIME)
+function displayItems(res) {
+    var arr = [];
+    res.forEach(element => {
+        arr.push(element.item_id + " | " + element.product_name + " | " + element.price);
+    });
+    return arr;
+}
+
+
 
 
 
